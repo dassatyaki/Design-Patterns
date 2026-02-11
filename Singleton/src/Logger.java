82aafeb22c765e2b63ca170e1e98075c99ee58b4
Satyaki Das <satyaki1@MacBook-Pro.local>
2026-02-11 22:00:51 +0530

    Add double locking

--- Singleton/src/Logger.java
+++ Singleton/src/Logger.java
@@ -1,13 +1,15 @@
 public class Logger {
-    private static Logger instance;
+    private static volatile Logger instance;
 
     private Logger(){};
 
     public static Logger getInstance()
     {
+        if(instance == null) {
         synchronized (Logger.class) {
             if (instance == null)
                 return new Logger();
+            }
         }
         return instance;
     }
